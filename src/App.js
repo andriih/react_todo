c78ae497fd58b5f84300ae711e9c546dbf468383
andriih <andrii.hnatyshyn@gmail.com>
2017-05-23 12:01:34 +0300

    basic structura added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TodoForm} from './components/todo/TodoForm.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {id:1 , name:'Learn JSX' , isComplete:true },
+        {id:2 , name:'Build an awesom apps!' , isComplete:false},
+        {id:3 , name:'Ship it!' , isComplete:false}
+       ],
+       currentTodo: ''
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+  handleInputChange(evt){
+    this.setState({
+      currentTodo : evt.target.value
+    })
+    
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Todos</h2>
+          <div className='Todo-App'>
+            <TodoForm handleInputChange={this.handleInputChange} currentTodo={this.state.currentTodo} />
+            <div className='Todo-List'>
+              <ul>
+                {this.state.todos.map(todo =>
+                  <li key={todo.id}>
+                    <input type='checkbox' defaultChecked={todo.isComplete}/>{todo.name}
+                  </li> )}
+              </ul>
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
